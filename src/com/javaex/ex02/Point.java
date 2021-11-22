2dab0ec5d9e26d908afaa30c005f5b3426330154
Hojun_Yoo <lisiee0@naver.com>
2021-11-22 18:06:20 +0900

    ex point

---
+++ src/com/javaex/ex02/Point.java
@@ -0,0 +1,23 @@
+package com.javaex.ex02;
+
+public class Point {
+	
+	private int x;
+	private int y;
+	
+	public void setX(int xp) {
+		x= xp;
+	}
+	public int getX() {
+		return x;
+	}
+	public void setY(int yp) {
+		y= yp;
+	}
+	public int getY() {
+		return y;
+	}
+	public void draw() {
+		System.out.println("점[x="+x+", y="+y+"]을 그렸습니다.");
+	}
+}
---
+++ src/com/javaex/ex02/PointApp.java
@@ -0,0 +1,28 @@
+package com.javaex.ex02;
+
+public class PointApp {
+	
+	public static void main(String[] args) {
+		
+		Point p1= new Point();
+		
+		p1.setX(5);
+		p1.setY(5);
+		
+		p1.draw();
+		
+		Point p2= new Point();
+		
+		p2.setX(10);
+		p2.setY(23);
+		
+		p2.draw();
+		
+		Point p3= new Point();
+		
+		p3.setX(44);
+		p3.setY(7);
+		
+		p3.draw();
+	}
+}
