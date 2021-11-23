1d5f9962ac64a6c21edc8ab36ba3b2bcc08d1674
Hojun_Yoo <lisiee0@naver.com>
2021-11-23 17:07:42 +0900

    11-23

--- src/com/javaex/ex02/PointApp.java
+++ src/com/javaex/ex02/PointApp.java
@@ -4,24 +4,24 @@ public class PointApp {
 	
 	public static void main(String[] args) {
 		
-		Point p1= new Point();
+		Point p1= new Point(6, 5);
 		
-		p1.setX(5);
-		p1.setY(5);
+		// p1.setX(5);
+		// p1.setY(5);
 		
 		p1.draw();
 		
-		Point p2= new Point();
+		Point p2= new Point(11, 23);
 		
-		p2.setX(10);
-		p2.setY(23);
+		// p2.setX(10);
+		// p2.setY(23);
 		
 		p2.draw();
 		
-		Point p3= new Point();
+		Point p3= new Point(24, 7);
 		
-		p3.setX(44);
-		p3.setY(7);
+		// p3.setX(44);
+		// p3.setY(7);
 		
 		p3.draw();
 	}
