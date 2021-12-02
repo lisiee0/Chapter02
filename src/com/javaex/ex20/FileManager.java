e5110d3186137b9967822db6650bd15bc4b9d39b
Hojun_Yoo <lisiee0@naver.com>
2021-12-02 15:11:36 +0900

    12-02

---
+++ src/com/javaex/ex20/FileManager.java
@@ -0,0 +1,21 @@
+package com.javaex.ex20;
+
+import java.io.IOException;
+
+public class FileManager {
+	
+	public void readFile() throws IOException { // C://config.txt 를 읽어오는 메소드(가정)
+		
+		
+		/*
+		try {
+			System.out.println("강제 예외상화발생");
+			throw new IOException(); // 파일을 못 읽었을대 예외 발생
+		}
+		catch (IOException e) {
+			System.out.println("파일이 없습니다.");
+		}
+		 */
+	}
+	
+}
